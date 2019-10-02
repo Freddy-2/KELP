1cbb359550b3c81661030532b0864c4921f2ea77
Alfred <alfred.allegretti@gmail.com>
2019-10-02 09:45:15 -0400

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resources :user, only: [:create]
+    resources :session, only: [:create. :destroy]
+
 
   root to: "static_pages#root"
 end
