f706fead1a9f2461f5349f2e00f0128a4e37053b
Alfred Allegretti <alfred.allegretti@gmail.com>
2019-10-02 09:52:53 -0400

    add jbuilder for user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :user, only: [:create]
     resources :session, only: [:create. :destroy]
+  end
 
 
   root to: "static_pages#root"
