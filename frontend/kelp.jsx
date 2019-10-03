fd07d5f7004712d50c9e757b432072c71cf66211
Alfred <alfred.allegretti@gmail.com>
2019-10-02 21:05:25 -0400

    user now stays logged in on refresh

--- frontend/kelp.jsx
+++ frontend/kelp.jsx
@@ -4,9 +4,18 @@ import configureStore from './store/store'
 import Root from './components/root'
 
 document.addEventListener("DOMContentLoaded", () => {
-
-  let store = configureStore();
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   ReactDOM.render(<Root store={store}/>, document.getElementById("root"));
 });
